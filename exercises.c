8c388bd35a299ed0e428f4fdeff91e625f1babcc
Your Name <you@example.com>
2025-03-19 09:00:20 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -48,8 +48,8 @@ List* crea_lista() {
 
    first(L);
    for (int i = 0 ; i < 10 ; i++){ 
-      int valor = i + 1;
-      pushBack(L, valor);
+      int *valor = i + 1;
+      pushBack(L, &valor);
       //usar pushback y first
    }
    return L;
