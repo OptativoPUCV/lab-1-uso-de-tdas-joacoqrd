bd841592f98e692dd7c927b3cbe06605796be469
Your Name <you@example.com>
2025-03-19 09:10:35 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -43,10 +43,6 @@ Al finalizar retorna la lista creada.
 
 List* crea_lista() {
    List* L = create_list(); 
-
-   int *num; //puntero a los elementos que guardaremos
-
-   first(L);
    for (int i = 0 ; i < 10 ; i++){ 
       int* num = (int*) malloc (sizeof(int));
       *(num) = i;
